e20fccda9203498d6e2de152b6d3ca19903cce50
tonycarballeira <tonycarballeira@gmail.com>
2015-01-31 21:30:03 -0500

    Add Model For Tickets

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,7 @@
 class ProjectsController < ApplicationController
+before_action :set_project, only: [:show,:edit,:update,:destroy]
+
+has_many :tickets
 
 	def new
 		@project = Project.new
@@ -7,7 +10,7 @@ class ProjectsController < ApplicationController
 	end
 
 	def edit
-		@project = Project.find(params[:id])
+		
 	end
 
 	def index
@@ -28,7 +31,7 @@ class ProjectsController < ApplicationController
 	end
 
 		def update
-			@project = Project.find(params[:id])
+			
 			
 			@project.update(project_params)
 				flash[:notice] = "Project has been updated."
@@ -36,7 +39,7 @@ class ProjectsController < ApplicationController
 		end
 
 		def destroy
-			@project = Project.find(params[:id])
+			
 			@project.destroy
 				
 				flash[:notice] = "Project has been destroyed."
@@ -48,6 +51,14 @@ class ProjectsController < ApplicationController
 	end
 
 	private
+
+	def set_project
+		@project = Project.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+		flash[:alert] = "The project you were looking" +
+						" for could not be found."
+		redirect_to projects_path
+	end
 	
 	def project_params
 		params.require(:project).permit(:name, :description)
