d923d2f606d0abf359791dbfee9bdab8e1c35fba
tonycarballeira <tonycarballeira@gmail.com>
2015-02-03 14:33:40 -0500

    Add Destroy Action to Tickets Controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,6 +7,9 @@ class TicketsController < ApplicationController
 		@ticket = @project.tickets.build
 	end
 
+	def edit
+	end
+
 	def create
 		@ticket = @project.tickets.build(ticket_params)
 			if @ticket.save
@@ -16,6 +19,23 @@ class TicketsController < ApplicationController
 				flash[:alert] = "Ticket has not been created."
 				render "new"
 			end
+	end
+
+	def update
+		if @ticket.update(ticket_params)
+			flash[:notice] = "Ticket has been updated."
+			redirect_to [@project, @ticket]
+		else
+			flash[:alert] = "Ticket has not been updated."
+			render action: "edit"
+		end
+	end
+
+	def destroy
+		@ticket.destroy
+		flash[:notice] = "Ticket has been deleted."
+		
+		redirect_to @project
 	end
 
 
